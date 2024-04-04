5085d746ae4f31b98eb568742aebe0c35c8326ea
Alessandro Campo <alessandrocampo97@gmail.com>
2024-04-04 15:10:35 +0200

    successful first api call

--- src/stores/generalStore.js
+++ src/stores/generalStore.js
@@ -1,15 +1,20 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useGeneralStore = defineStore('generalStore', {
   state: () => ({
-
-
+    projects_array: []
   }),
   getters: {
 
   },
   actions: {
-
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then(res => {
+        console.log(res.data)
+        this.projects_array = res.data
+      })
+    }
   },
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup>
+import { useGeneralStore } from '../stores/generalStore'
+import { ref, onMounted } from 'vue'
+const store = useGeneralStore()
 
+onMounted(() => {
+  store.getProjects()
+  console.log(store.projects_array)
+})
 </script>
 
 <template>
